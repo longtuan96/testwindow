b42d006372557911611c6115e7d312425d64f5f6
longtuan96 <longtuan03@gmail.com>
2021-10-06 15:37:27 +0700

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,35 @@
+import React, { FC } from 'react';
+import logo from './logo.svg';
+
+import './App.css';
+import { Button } from 'antd';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.tsx</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+const App :FC = () => {
+  return <div>
+    <Button type="primary">
+      Button 
+    </Button>
+  </div>;
+}
+
+export default App;
